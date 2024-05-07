b67d46babdd51204e4bef6f74c632592cc1a53f7
Jaystarz360 <Jaystarz360@gmail.com>
2024-05-07 02:37:56 -0400

    Update questions

---
+++ util/questions.js
@@ -0,0 +1,38 @@
+const question = [{
+  
+    type: 'list',
+    name: 'task',
+    message: "Please select a task:",
+    choices: [
+      {
+        name:'View all departments',
+      },
+      {
+        name:'View all roles',
+      },
+      {
+       name:'View all employees',
+      },
+      {
+        name:'Add a department',
+       },
+       {
+        name:'Add a role',
+       },
+       {
+        name:'Add an employee',
+       },
+       {
+        name:'Update an employee role',
+       },
+       {
+        name:'View employees by department',
+       },
+       {
+        name:'View the budget for a department',
+       },
+       {
+        name:'Exit',
+       }
+  ]
+  }];
