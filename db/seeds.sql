acd401a0faf77bf1745fb4e8872754c8cc559782
Jaystarz360 <Jaystarz360@gmail.com>
2024-05-07 19:59:59 -0400

    Update seeds + sever

---
+++ db/seeds.sql
@@ -0,0 +1,16 @@
+INSERT INTO dept_tbl (id, dept_name)
+VALUES 
+
+SELECT * FROM dept_tbl;
+
+INSERT INTO role_tbl (id, title, salary, dept_id)
+VALUES 
+
+SELECT * FROM role_tbl;
+
+INSERT INTO emp_tbl (id, first_name, last_name, role_id, manager_id)
+VALUES  
+
+SELECT * FROM emp_tbl;
+SELECT 
+
