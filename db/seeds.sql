ee65bd1e74f15171c508b5f704ee8bd6bfdc8146
Jaystarz360 <Jaystarz360@gmail.com>
2024-05-07 22:21:35 -0400

    Update seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,11 @@
 INSERT INTO dept_tbl (id, dept_name)
-VALUES 
+VALUES (10, "Power Weapons"),
+       (11, "Power Armors"),
+       (12, "Power Sheilds"),
+       (13, "Fusion Core"),
+       (14, "Orbital Specialist"),
+       (15, "Vehicle Specialist"),
+       (16, "Laundry");
 
 SELECT * FROM dept_tbl;
 
